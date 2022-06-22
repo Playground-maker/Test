c84d7bc572a743a322352753619f8557afa6181d
김동오 <dongo1450@gmail.com>
2022-06-23 04:26:56 +0900

    Studying 1

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import _ from 'lodash';
+/* import _ from 'lodash';
+ console.log('HEROPY');
+console.log(_.camelCase('HEROPY')); 
 
-console.log('HEROPY');
-console.log(_.camelCase('HEROPY'));
+let myName = "HEROPY";
+let email = 'dongo1450@gmail.com';
+let hello = `Hello ${myName}?!`;
+
+console.log(myName);
+console.log(email);
+console.log(hello); */
+
+let user = {
+    name: 'HEROPY',
+    age: 85
+
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.name);
+
